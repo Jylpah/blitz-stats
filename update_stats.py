34c481dd4a8bce7e59a517c8bd93e49d0ba017eb
Jylpah <jylpah@gmail.com>
2019-12-28 02:19:38 +0200

    progress bar support to BS_tank_stat_worker()

--- update_stats.py
+++ update_stats.py
@@ -137,7 +137,8 @@ async def main(argv):
 		for mode in set(args.mode) & set(UPDATE_FIELD.keys()):
 			tmp_progress_max += len(active_players[mode])
 		bu.print_new_line()
-		bu.set_progress_bar('Fetching stats', tmp_progress_max)
+		bu.set_progress_bar('Fetching stats', tmp_progress_max, 1000)
+
 		for mode in UPDATE_FIELD:
 			Q[mode] = asyncio.Queue()
 		
@@ -499,6 +500,7 @@ async def BS_tank_stat_worker(db : motor.motor_asyncio.AsyncIOMotorDatabase, pla
 						stats_added += tmp								
 					finally:
 						await update_stats_update_time(db, account_id, field, last_battle_time)
+						bu.print_progress()
 						bu.debug('Added stats for account_id=' + str(account_id), worker_id)	
 
 		except Exception as err:
