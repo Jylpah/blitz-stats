70c4e3f81dd5cbc0cdb93fb2ce4ea6e4969b2647
Jylpah <jylpah@gmail.com>
2020-01-15 09:07:29 +0200

    Allow disk cache in export_stats.py

--- export_stats.py
+++ export_stats.py
@@ -290,7 +290,7 @@ async def q_tank_stats_WG(workerID: int, db: motor.motor_asyncio.AsyncIOMotorDat
                                             'doc': {'$first': '$$ROOT'}}},
                                 {'$replaceRoot': {'newRoot': '$doc'}}, 
                                 {'$project': {'_id': False}} ]
-                        cursor = dbc.aggregate(pipeline, allowDiskUse=False)
+                        cursor = dbc.aggregate(pipeline, allowDiskUse=True)
                     
                     async for doc in cursor:
                         bu.print_progress()
