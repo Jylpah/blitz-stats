18b955a3e32c0614ebf0fcb87accaabc846ce5f8
Jylpah <jylpah@gmail.com>
2022-11-08 01:42:20 +0200

    cmd_accounts_fetch_wi(): ready for testing

--- accounts.py
+++ accounts.py
@@ -1,12 +1,14 @@
 from argparse import ArgumentParser, Namespace
 from configparser import ConfigParser
-from typing import Optional
+from typing import Optional, cast
 import logging
-from asyncio import Queue
+from asyncio import create_task, gather, Queue, CancelledError, Task
+from aiohttp import ClientResponse
 
 from backend import Backend, OptAccountsInactive
 from models import Account
 from pyutils.eventcounter import EventCounter
+from pyutils.utils import get_url, get_url_JSON_model, epoch_now
 from blitzutils.models import WoTBlitzReplayJSON, Region
 from blitzutils.wotinspector import WoTinspector
 
@@ -17,9 +19,11 @@ verbose	= logger.info
 debug	= logger.debug
 
 WI_MAX_PAGES 	: int 				= 100
+WI_MAX_OLD_REPLAYS: int 			= 30
 WI_RATE_LIMIT	: Optional[float] 	= None
 WI_AUTH_TOKEN	: Optional[str] 	= None
-ACCOUNTS_Q_MAX 	: int				= 5000
+ACCOUNTS_Q_MAX 	: int				= 100
+ACCOUNT_Q_MAX 	: int				= 5000
 
 ###########################################
 # 
@@ -76,16 +80,18 @@ def add_args_accounts_fetch(parser: ArgumentParser, config: Optional[ConfigParse
 
 def add_args_accounts_fetch_wi(parser: ArgumentParser, config: Optional[ConfigParser] = None) -> bool:
 	try:
+		global WI_MAX_OLD_REPLAYS
+
 		WI_RATE_LIMIT 	: float  		= 20/3600
 		WI_MAX_PAGES 	: int 			= 100
 		WI_AUTH_TOKEN 	: Optional[str] = None
-		WI_WORKERS		: int 			= 2
+		WI_WORKERS		: int 			= 2		
 
-		if config is not None and 'WI' in config.sections():
-			configWI 		= config['WI']
+		if config is not None and 'WOTINSPECTOR' in config.sections():
+			configWI 		= config['WOTINSPECTOR']
 			WI_RATE_LIMIT	= configWI.getfloat('rate_limit', WI_RATE_LIMIT)
 			WI_MAX_PAGES	= configWI.getint('max_pages', WI_MAX_PAGES)
-			WI_WORKERS		= configWI.getint('threads', WI_WORKERS)
+			WI_WORKERS		= configWI.getint('workers', WI_WORKERS)
 			WI_AUTH_TOKEN	= configWI.get('auth_token', WI_AUTH_TOKEN)
 
 		parser.add_argument('--max', '--max-pages', dest='wi_max_pages', 
@@ -97,6 +103,9 @@ def add_args_accounts_fetch_wi(parser: ArgumentParser, config: Optional[ConfigPa
 		parser.add_argument('--threads', '--workers', dest='wi_workers', 
 							type=int, default=WI_WORKERS, metavar='WORKERS',
 							help='Number of async workers to spider wotinspector.com')
+		parser.add_argument('--old-replay-limit', dest='wi_max_old_replays', 
+							type=int, default=WI_MAX_OLD_REPLAYS, metavar='OLD-REPLAYS',
+							help='Cancel spidering after number of old replays found')
 		parser.add_argument('--wi-auth-token', dest='wi_auth_token', 
 							type=str, default=WI_AUTH_TOKEN, metavar='AUTH_TOKEN',
 							help='Start page to start spidering of WoTinspector.com')
@@ -210,14 +219,28 @@ async def cmd_accounts_fetch(db: Backend, args : Namespace, config: Optional[Con
 		# close backend + queue
 		# print stats
 		stats = EventCounter('accounts fetch')
-		accountQ : Queue = Queue(maxsize=ACCOUNTS_Q_MAX)
+		accountQ : Queue[list[int]] = Queue(maxsize=ACCOUNTS_Q_MAX)
+		db_worker = create_task(accounts_add_worker(db, accountQ))
 
-		if args.accounts_fetch_source == 'wi':
-			debug('wi')
-			await cmd_accounts_fetch_wi(db, args, accountQ, stats, config)
-		elif args.accounts_fetch_source == 'files':
-			debug('files')
-			await cmd_accounts_fetch_files(db, args, accountQ, stats, config)
+		try:
+			if args.accounts_fetch_source == 'wi':
+				debug('wi')
+				stats.merge_child(await cmd_accounts_fetch_wi(db, args, accountQ, config))
+			elif args.accounts_fetch_source == 'files':
+				debug('files')
+				stats.merge_child(await cmd_accounts_fetch_files(db, args, accountQ, config))
+		except Exception as err:
+			error(str(err))
+
+		await accountQ.join()
+		db_worker.cancel()
+		worker_res : tuple[EventCounter | BaseException] = await gather(db_worker,return_exceptions=True)
+		for res in worker_res:
+			if type(res) is EventCounter:
+				stats.merge_child(res)
+			elif type(res) is BaseException:
+				error(f'Backend ({db.name}) add_accounts_worker() returned error: {str(res)}')
+
 		stats.print()
 
 	except Exception as err:
@@ -225,40 +248,161 @@ async def cmd_accounts_fetch(db: Backend, args : Namespace, config: Optional[Con
 	return False
 
 
-async def cmd_accounts_fetch_files(db: Backend, args : Namespace, queue : Queue, stats: EventCounter, 
-									config: Optional[ConfigParser] = None) -> EventCounter:
-	
-	raise NotImplementedError
+async def accounts_add_worker(db: Backend, accountQ: Queue[list[int]]) -> EventCounter:
+	"""worker to read accounts from queue and add those to backend"""
+	stats : EventCounter = EventCounter(f'Backend {db.name}')
+	added 		: int
+	not_added 	: int
 	try:
 		debug('starting')
-		
-		
-
-
+		while True:
+			players : list[int] = await accountQ.get()
+			try:
+				debug(f'Read {len(players)} from queue')
+				stats.log('candidates', len(players))
+				try:
+					accounts : list[Account] = list()
+					for player in players:
+						try:
+							accounts.append(Account(id=player, added=epoch_now()))
+						except Exception as err:
+							error(f'cound not create account object for account_id: {player}')
+					added, not_added= await db.accounts_insert(accounts)
+					stats.log('added', added)
+					stats.log('not added', not_added)
+				except Exception as err:
+					stats.log('errors')
+					error(f'Cound not add accounts do {db.name}: {str(err)}')
+			except Exception as err:
+				error(str(err))
+			finally:
+				accountQ.task_done()
+	except CancelledError as err:
+		debug(f'Cancelled')
 	except Exception as err:
 		error(str(err))
 	return stats
 
 
-async def cmd_accounts_fetch_wi	(db: Backend, args : Namespace, queue : Queue, stats: EventCounter,
+async def cmd_accounts_fetch_files(db: Backend, args : Namespace, accountQ : Queue[list[int]], 
+									config: Optional[ConfigParser] = None) -> EventCounter:
+	
+	debug('starting')
+	raise NotImplementedError
+
+
+async def cmd_accounts_fetch_wi	(db: Backend, args : Namespace, accountQ : Queue[list[int]],
 									 config: Optional[ConfigParser] = None) -> EventCounter:
 	"""Fetch account_ids from replays.wotinspector.com replays"""
-	try:
-		debug('starting')
-		workers		: int 	= args.wi_workers
-		max_pages	: int	= args.wi_max_pages
-		start_page 	: int 	= args.wi_start_page
-		rate_limit 	: float	= args.wi_rate_limit
-		force 		: bool  = args.force
-		token		: str 	= args.wi_auth_token	# temp fix...
-		replayQ  	: Queue = Queue()
-		wi 			: WoTinspector 	= WoTinspector(rate_limit=rate_limit, auth_token=token)
+	debug('starting')
+	stats		: EventCounter = EventCounter('WoTinspector')
+	
+	workersN	: int 	= args.wi_workers
+	workers		: list[Task] = list()
+	max_pages	: int	= args.wi_max_pages
+	start_page 	: int 	= args.wi_start_page
+
+	rate_limit 	: float	= args.wi_rate_limit
+	force 		: bool  = args.force
+	token		: str 	= args.wi_auth_token	# temp fix...
+	replay_idQ  : Queue[str] = Queue()
+	# pageQ		: Queue[int] = Queue()
+	wi 			: WoTinspector 	= WoTinspector(rate_limit=rate_limit, auth_token=token)	
+
+	try:					
+		step : int = 1
+		if max_pages < 0:
+			step = -1
+		elif max_pages == 0:
+			step = -1
+			max_pages = - start_page
 		
-		
-		
+		pages : range = range(start_page,(start_page + max_pages), step)
+
+		stats.merge_child(await accounts_fetch_wi_spider_replays(db, wi, args, replay_idQ,  pages))
+
+		for _ in range(workersN):
+			workers.append(create_task(accounts_fetch_wi_fetch_replays(db, wi, replay_idQ, accountQ )))
+		await replay_idQ.join()
+
+		for worker in await gather(*workers, return_exceptions=True):
+			stats.merge_child(worker)
 
 	except Exception as err:
 		error(str(err))
+	finally:
+		await wi.close()
+	return stats
+
+
+async def accounts_fetch_wi_spider_replays(db: Backend, wi: WoTinspector, args : Namespace, 
+											replay_idQ : Queue[str], pages : range) -> EventCounter:
+	"""Spider replays.WoTinspector.com and feed found replay IDs into replayQ. Return stats"""
+	stats : EventCounter = EventCounter('Crawler')	
+	max_old_replays : int= args.wi_max_old_replays
+	force 		: bool  = args.force
+	old_replays : int = 0
+	
+	try:
+		debug(f'Starting ({len(pages)} pages)')
+		for page in pages:
+			try:
+				if old_replays > max_old_replays:
+					message(f'{max_old_replays} found. Stopping spidering for more')
+					break			
+				url : str = wi.get_url_replay_listing(page)
+				resp : str | None = await get_url(wi.session, url)
+				if resp is None:
+					error('could not spider replays.WoTinspector.com page {page}')
+					stats.log('error')
+					continue
+				debug(f'HTTP request OK')
+				replay_ids : set[str] = wi.parse_replay_ids(resp)
+				debug(f'Page {page}: {len(replay_ids)} found')
+				if len(replay_ids) == 0:					
+					break
+				for replay_id in replay_ids:
+					if not force:
+						res : WoTBlitzReplayJSON | None = await db.replay_get(replay_id)
+						if res is not None:
+							debug(f'Replay already in the {db.name}: {replay_id}')
+							old_replays += 1 
+							stats.log('old replay')					
+							continue
+					await replay_idQ.put(replay_id)
+					stats.log('new replay')
+			except Exception as err:
+				error(str(err))
+	except CancelledError as err:
+		debug(f'Cancelled')
+	except Exception as err:
+		error(str(err))
+	return stats
+
+
+async def accounts_fetch_wi_fetch_replays(db: Backend, wi: WoTinspector, replay_idQ : Queue[str], 
+											accountQ : Queue[list[int]]) -> EventCounter:
+	stats : EventCounter = EventCounter('Fetch replays')
+	try:
+		while not replay_idQ.empty():
+			replay_id = await replay_idQ.get()
+			try:
+				url : str = wi.get_url_replay_JSON(replay_id)
+				replay : WoTBlitzReplayJSON | None = cast(WoTBlitzReplayJSON, await get_url_JSON_model(wi.session, url, WoTBlitzReplayJSON ))
+				if replay is None:
+					error(f'Could not fetch replay id: {replay_id}')
+					continue
+				players : list[int] = replay.get_players()
+				stats.log('players found', len(players))
+				await accountQ.put(players)
+				if await db.replay_insert(replay):
+					stats.log('replays added')
+				else:
+					stats.log('replays not added')
+			finally:
+				replay_idQ.task_done()
+	except Exception as err:
+		error(str(err))	
 	return stats
 
 
