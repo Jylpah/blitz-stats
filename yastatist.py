6c749c586597108c7898c9a6fbda550a606b08e2
Jylpah <jylpah@gmail.com>
2023-02-09 14:39:35 +0200

    get_accounts_since(): working and tested

--- yastatist.py
+++ yastatist.py
@@ -2,6 +2,7 @@ from aiohttp import ClientSession, ClientTimeout
 import logging
 from datetime import timedelta, datetime, timezone
 from typing import AsyncGenerator 
+from http import HTTPStatus
 
 from pyutils.utils import JSONExportable
 from blitzutils.models import Region
@@ -14,9 +15,6 @@ error 	= logger.error
 message	= logger.warning
 verbose	= logger.info
 debug	= logger.debug
-
-
-logging.basicConfig(level=logging.DEBUG)
 
 class YastatistAccount(JSONExportable):
 	id: int
@@ -32,26 +30,24 @@ async def get_accounts_since(region: Region,
 	assert days > 0, "days has to be positive integer"
 	try:
 		since : datetime = datetime.now(tz=timezone.utc) - timedelta(days=days)
-		headers : dict[str, str] = {'connection': 'keep-alive', 
-									'content-type': 'application/json', 
-									'transfer-encoding': 'chunked', 
-									'CF-Access-Client-Id': client_id,
+		headers : dict[str, str] = {'CF-Access-Client-Id': client_id,
 									'CF-Access-Client-Secret': secret
 									}
 		url : str = f"/api/{region.value}/accounts/{since.isoformat(timespec='seconds')}/active-since"
-		print(url)
-		timeout = ClientTimeout(total=10)
+		timeout = ClientTimeout(sock_read=20)
 		async with ClientSession(base_url=YS_BASE_URL, headers=headers, timeout=timeout) as session:
-			print('session')
 			async with session.get(url) as resp:
-				print('response')
-				async for line in resp.content:
-					print(f'new line')
-					try:
-						account = YastatistAccount.parse_raw(line)
-						yield BSAccount(id=account.id, region=region)
-					except Exception as err:
-						error(f'{err}')
+				status : HTTPStatus = HTTPStatus(resp.status)
+				if resp.ok:
+					debug(f'HTTP GET: {status.value} / {status.phrase}')
+					async for line in resp.content:
+						try:
+							account = YastatistAccount.parse_raw(line)
+							yield BSAccount(id=account.id, region=region)
+						except Exception as err:
+							error(f'Could not read line: {err}')
+				else:
+					error(f'GET {url}: {status.value} / {status.phrase}')
 
 	except Exception as err:
 		error(f'{err}')
